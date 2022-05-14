f8bad4cb641aa55ee97acbb5dbaa7fa7b896baba
AftabUfaq <engr.aftabufaq@gmail.com>
2022-05-14 21:26:50 +0500

    User Register Dopne

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require('express')
 var router = express.Router();
-import User from '../Schema/userSchema'
-/* GET users listing. */
-router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+const User =  require('../Schema/userSchema')
+
+// Register SUer
+router.post('/', async (req, res, next) => {
+  const name = req.body.nameuser;
+  const cnic = req.body.cnic;
+  const mobile = req.body.mobile;
+  const type = req.body.type;
+  const email = req.body.email;
+  const profile_pic = req.body.profile_pic;
+  const password = req.body.password;
+
+
 });
 
 module.exports = router;
